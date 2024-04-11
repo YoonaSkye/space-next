752c789e59b59998a90c6de852c18ea96e275764
Sheldon Liao <sheldon.liao1015@gmail.com>
2024-04-11 14:08:43 +0800

    feat: :sparkles: 增加command line滚动效果, 增加help提示

--- components/command/command-line.tsx
+++ components/command/command-line.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import { CLEAR } from '@/constants'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import CommandInput from './command-input'
 import CommandOutput from './command-output'
 
-export default function CommandLine() {
+export default function CommandLine(props: Record<string, any>) {
   // 当前点击的cmd
   const [currentClickCmd, setCurrentClickCmd] = useState('')
   const containerRef = useRef<HTMLDivElement>(null)
@@ -16,17 +16,41 @@ export default function CommandLine() {
     setTypedCmds(cmd === CLEAR ? [] : (prev) => [...prev, cmd])
   }
 
+  // 出现滚动条自动滚动到可视区域底部
+  useEffect(() => {
+    if (props.scroll) {
+      props.scroll()
+    } else {
+      containerRef.current?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end'
+      })
+    }
+    // eslint-disable-next-line
+  }, [typedCmds])
+
   return (
     <div ref={containerRef} className="pt-10 pb-16">
-      {typedCmds.length &&
-        typedCmds.map(
-          (cmd, index) =>
-            cmd && <CommandOutput key={`${cmd}-${index}`} cmd={cmd} />
-        )}
+      {typedCmds.length
+        ? typedCmds.map(
+            (cmd, index) =>
+              cmd && <CommandOutput key={`${cmd}-${index}`} cmd={cmd} />
+          )
+        : ''}
       <CommandInput
         currentClickCmd={currentClickCmd}
         onTypingCmd={onTypingCmd}
       />
+
+      {typedCmds.length === 2 ? (
+        <div className="mt-3 text-slate-400">
+          输入
+          <button className="mx-2 text-sky-500">help</button>
+          查看更多命令
+        </div>
+      ) : (
+        ''
+      )}
     </div>
   )
 }
