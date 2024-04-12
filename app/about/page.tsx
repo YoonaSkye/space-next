eb59e5ad0720a282ef185d56b0a40fd4322231b8
Sheldon Liao <sheldon.liao1015@gmail.com>
2024-04-12 13:46:23 +0800

    feat: :sparkles: about page content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,18 @@
 export default function AboutPage() {
-  return <div>AboutPage</div>
+  return (
+    <article className="prose">
+      <h2>👋 Welcome</h2>
+      <h3>我</h3>
+      <p>
+        我是 <span className="text-cyan-400 font-bold">yoonaskye</span>
+        ，一名前端开发，对CLI工具开发感兴趣，偶尔在空闲时间开发一些小玩意
+      </p>
+      <p>喜欢音乐、旅行、宇宙、摄影but业余，也会偶尔打打游戏，生活简单且自由</p>
+      <p>做好自己热爱的事，想法很多 Just Do!~</p>
+
+      <h3>网站</h3>
+      <p>博客内容以技术分享、个人想法、生活记录为主</p>
+      <p>主技术栈：Next.js + React + TypeScript + TailwindCSS</p>
+    </article>
+  )
 }
